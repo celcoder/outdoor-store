4b8f282b8ecdeae9420641f2dd9d42cbf6c8a68f
Reed Branson <reed.branson@gmail.com>
2016-07-26 16:15:09 -0400

    finished user factory, time for PR/code review

--- browser/js/user-details/user.js
+++ browser/js/user-details/user.js
@@ -1,1 +1,28 @@
-user.js
+app.factory('User', function($http) {
+
+  function getData(res) { return res.data; }
+
+  var User = {};
+
+  User.fetchById = function(id) {
+    return $http.get('/api/user/' + id)
+      .then(getData);
+  };
+
+  User.deleteUser = function(id) {
+    return $http.delete('/api/user/' + id);
+  }
+
+  User.createUser = function(data) {
+    return $http.post('/api/user/', data)
+      .then(getData);
+  }
+
+  User.modifyUser = function(id, data) {
+    return $http.put('/api/user/' + id, data)
+      .then(getData);
+  }
+
+  return User;
+
+})
