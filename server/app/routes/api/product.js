323bdb6811d068d392a2e7ee29716ab37ec0d053
Chris Heller <ctheller12@gmail.com>
2016-07-26 16:10:24 -0400

    routes for product

---
+++ server/app/routes/api/product.js
@@ -0,0 +1,50 @@
+var db = require('../../db');
+var Product = db.model('product');
+var router = require('express').Router();
+
+//get Product by id
+router.get('/:id', function(req,res,next){
+	Product.findOne({where: {id: req.params.id}})
+	.then(function(returnedProduct){
+		return res.status(200).send(returnedProduct);
+	})
+	.catch(next);
+});
+
+//get all products by status
+router.get('/:status', function(req,res,next){
+	Product.findAll({where: {status: req.params.status}})
+	.then(function(selectedProducts){
+		return res.status(200).send(selectedProducts);
+	})
+	.catch(next);
+})
+
+//get all Products
+router.get('/', function(req,res,next){
+	Product.findAll({})
+	.then(function(allProducts){
+		return res.status(200).send(allProducts);
+	})
+	.catch(next);
+});
+
+//Add a product
+router.post('/', function(req,res,next){
+	Product.create(req.body)
+	.then(function(createdProduct){
+		return res.status(201).send(createdProduct);
+	})
+	.catch(next);
+});
+
+//Update a product's stock and/or status where req.body = {status: 'out of stock', stock: 0}
+router.put('/:id', function(req,res,next){
+	Product.update(req.body, {where: {id: req.params.id}, returning:true})
+	.then(function(updatedProduct){
+		return res.status(200).send(updatedProduct);
+	})
+})
+
+
+
