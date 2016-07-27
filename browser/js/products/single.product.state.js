2a214af5cd358479c3e1c7b1bd385c260b417d71
Chris Heller <ctheller12@gmail.com>
2016-07-27 14:40:42 -0400

    single product page

--- browser/js/products/single.product.state.js
+++ browser/js/products/single.product.state.js
@@ -11,10 +11,26 @@ app.config(function($stateProvider) {
 
 app.controller('ProductCtrl', function ($scope, $state, $stateParams, ProductFactory, $log) {
 
- ProductFactory.fetchById($stateParams.id)
-    .then(function (product) {
-      $scope.product = product;
-    })
-    .catch($log);
+ 	ProductFactory.fetchById($stateParams.id)
+	    .then(function (product) {
+	      $scope.product = product;
+	    })
+	    .catch($log);
+
+	$scope.up = function(){
+		if ($scope.selectedNum) return $scope.selectedNum++;
+		else {
+			$scope.selectedNum = 2;
+			return;
+		}
+	}
+
+	$scope.down = function(){
+		if ($scope.selectedNum && $scope.selectedNum > 1) $scope.selectedNum--;
+		return;
+	}
+
+	//unfinished
+	$scope.addToCart = function(){};
 
 })
