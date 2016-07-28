142ee0cd1deacdfdc94e62a313ddd26a68a09776
Chris Heller <ctheller12@gmail.com>
2016-07-28 17:58:31 -0400

    subtotal

--- browser/js/order/order.js
+++ browser/js/order/order.js
@@ -14,4 +14,17 @@ app.config(function ($stateProvider) {
 app.controller('CartCtrl', function($scope, cart){
 	$scope.cart = cart;
 	$scope.seeOrders = function(){ console.log("THIS IS THE CART", cart)};
+	
+	//subtotal math
+	var prices = cart.products.map(function(product){
+		return parseFloat(product.price);
+	})
+	var quantities = cart.products.map(function(product){
+		return product.productOrder.quantity;
+	})
+	var subtotal = 0;
+	for (var i = 0; i<cart.products.length; i++){
+		subtotal += prices[i]*quantities[i];
+	}
+	$scope.subtotal = subtotal;
 })
