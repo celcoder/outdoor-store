44c3e6e9e38bea976826ccfb8ba2868b7e67d6d7
Chang Ethan Lee <celcoder@gmail.com>
2016-07-28 16:39:49 -0400

    homepage carousel

---
+++ browser/js/home/HomepagePics.js
@@ -0,0 +1,8 @@
+app.factory('HomePagePics', function () {
+    return [
+        "https://www.rei.com/assets/drsp/2016/q3/homepage/07-26/lead-brooks-md-lg/live.jpg",
+        "https://www.rei.com/assets/drsp/2016/q3/homepage/07-26/paddling/live.jpg",
+        "https://images.thenorthface.com/is/image/TheNorthFaceBrand/051116-activity-hero-climbing-d?$SCALE-ORIGINAL$",
+        "http://www.ems.com/on/demandware.static/-/Sites-EMS-Library/default/dw16d55e4f/images/promotions/homepage/2016/07/24/20160724_hm_box4.jpg"
+    ];
+});
