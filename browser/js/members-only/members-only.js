8acfbd6cb96699f15d87a3e7fd63c3db25afbaa3
Chang Ethan Lee <celcoder@gmail.com>
2016-07-29 15:45:24 -0400

    fixed subtotal

--- browser/js/members-only/members-only.js
+++ browser/js/members-only/members-only.js
@@ -1,33 +1,44 @@
-app.config(function ($stateProvider) {
+app.config(function($stateProvider) {
 
-    $stateProvider.state('membersOnly', {
-        url: '/user/:id',
-        templateUrl: '/js/members-only/account-info.html' ,
-        controller: 'UserCtrl',
-        resolve: {
-            user: function (UserFactory, $stateParams) {
-            return UserFactory.fetchById($stateParams.id)
-            },
-            orderHistory: function (OrderFactory, $stateParams) {
-                return OrderFactory.getUserHistory($stateParams.id)
-            },
-            cart: function (OrderFactory, $stateParams) {
-                return OrderFactory.getUserCart($stateParams.id)
-            }
-        },
-        // The following data.authenticate is read by an event listener
-        // that controls access to this state. Refer to app.js.
-        data: {
-            authenticate: true
-        }
-    });
+  $stateProvider.state('membersOnly', {
+    url: '/user/:id',
+    templateUrl: '/js/members-only/account-info.html',
+    controller: 'UserCtrl',
+    resolve: {
+      user: function(UserFactory, $stateParams) {
+        return UserFactory.fetchById($stateParams.id)
+      },
+      orderHistory: function(OrderFactory, $stateParams) {
+        return OrderFactory.getUserHistory($stateParams.id)
+      },
+      cart: function(OrderFactory, $stateParams) {
+        return OrderFactory.getUserCart($stateParams.id)
+      }
+    },
+    // The following data.authenticate is read by an event listener
+    // that controls access to this state. Refer to app.js.
+    data: {
+      authenticate: true
+    }
+  });
 
 });
 
-app.controller('UserCtrl', function ($scope, user, orderHistory, cart) {
+app.controller('UserCtrl', function($scope, user, orderHistory, cart) {
 
- $scope.user = user;
- $scope.orderHistory = orderHistory;
- $scope.cart = [cart];
+  $scope.user = user;
+  $scope.orderHistory = orderHistory;
+  $scope.cart = [cart];
+  var prices = cart.products.map(function(product) {
+    return parseFloat(product.price);
+  })
+  var quantities = cart.products.map(function(product) {
+    return product.productOrder.quantity;
+  })
+  var subtotal = 0;
+  for (var i = 0; i < cart.products.length; i++) {
+    subtotal += prices[i] * quantities[i];
+  }
+  $scope.cart[0].subtotal = subtotal;
 
 });
