4a07f380e0d04c2c930fd073979dea47f844b2bd
Reed Branson <reed.branson@gmail.com>
2016-08-01 11:12:42 -0400

    working on confirmation page

--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -16,10 +16,37 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('CheckoutCtrl', function ($scope, user, cart) {
+app.controller('CheckoutCtrl', function ($scope, user, cart, OrderFactory, $state) {
 
  $scope.user = user;
  $scope.cart = cart;
+ $scope.showShipping = false;
+
+ $scope.toggleShipping = function () {
+  $scope.showShipping = !$scope.showShipping;
+ }
+
+ $scope.submitOrder = function (id) {
+
+  $state.go('orderFulfilled');
+ }
+
+ $scope.cart.subtotal = 0;
+
+  // subtotal math
+  if (cart.products.length){
+    var prices = cart.products.map(function(product){
+      return parseFloat(product.price);
+    })
+    var quantities = cart.products.map(function(product){
+      return product.productOrder.quantity;
+    })
+    var subtotal = 0;
+    for (var i = 0; i<cart.products.length; i++){
+        subtotal += prices[i] * quantities[i];
+    }
+    $scope.cart.subtotal = subtotal;
+  }
 
 });
 
